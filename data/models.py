ab025e226899bce13580280e1868f33ea612eddf
Nathan Graule <solarliner@gmail.com>
2019-10-11 22:17:31 +0200

    Add frontend HTML

--- data/models.py
+++ data/models.py
@@ -7,16 +7,32 @@ from django.db import models
 # Create your models here.
 
 class BaseObject(models.Model):
+    class Meta:
+        abstract = True
     uid = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
 
 
 class User(BaseObject):
-    raw_user = models.OneToOneField(settings.AUTH_USER, related_name='data_user', on_delete=models.CASCADE)
-    levels_done = models.ManyToManyField('Level', related_name='done_users', on_delete=models.CASCADE)
+    raw_user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='data_user', on_delete=models.CASCADE)
+    levels_done = models.ManyToManyField('Level', related_name='done_users')
 
 
 class Level(BaseObject):
     name = models.CharField(max_length=100)
     needs = models.ManyToManyField('Level', related_name='depends')
+    data = models.TextField() # TODO: Chercher comment intégrer les données
+    tracking_allowed = models.BooleanField(default=True)
+    diagonal_allowed = models.BooleanField(default=True)
+
+    def done_for_user(self, user: User) -> bool:
+        return self.done_users.filter(uid=user.uid).exists()
+
+    def is_allowed_for_user(self, user: User) -> bool:
+        # Vérifie que tous les niveaux nécéssaires sont vérifiés
+        if self.needs.count() > 0:
+            return all(l.done_for_user(user) for l in self.needs.all())
+        else:
+            # Vérifier que les niveaux fait par l'utilisateur contient le niveau actuel
+            return True
--- data/views.py
+++ data/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
